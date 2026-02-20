db22a7804690fa8735f95103e0c63a75fd96b0bd
Dini90kid <dineshkrishnann@gmail.com>
2026-02-21 03:31:50 +0530

    Add files via upload

---
+++ dso.py
@@ -0,0 +1,4 @@
+
+def merge_into_delta(ctx):
+    """MERGE INTO Delta target replaces RSDRI_ODSO_* writes."""
+    pass
---
+++ logsys.py
@@ -0,0 +1,4 @@
+
+def map_logsys_from_id(ctx):
+    """Lookup logical system mapping via small UC table."""
+    pass
